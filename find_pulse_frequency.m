8f0a07eecf6f31faa0b58bf9ab6f8bfe092a99b1
bspatter <i.am.brandon.patterson@gmail.com>
2019-03-25 15:55:55 -0400

    find_pulse_frequency.m now uses interpolation to fill in extremely narrowband pulses.

--- find_pulse_frequency.m
+++ find_pulse_frequency.m
@@ -1,3 +1,4 @@
+
 function [pulse_frequency, varargout] = find_pulse_frequency(pulse_time,pulse_pressure)   
 % FIND_PULSE_FREQUENCY:
 % GOAL: Finds the dominant frequency of the pulse (pulse defined based on the FDA definition of pulse definition)
@@ -17,6 +18,9 @@ N = round(N/2)*2; %force it to be even
 TimeTotal = N/SamplingFrequency; %new total time domain length, if N changed
 TimeFFT = (0:(N-1))/SamplingFrequency; % time vector (max(t) is 1 bin short of T)
 FrequencySpectrum = (0:(N/2))/N*SamplingFrequency; %frequency vector (min(f)=0, the DC component. max(f) = fs/2 exactly)
+FrequencySpectrum = FrequencySpectrum(:);
+
+
 % Match pressure to output variables
 if length(TimeFFT)==length(pulse_pressure)
     y = pulse_pressure;%function(t); %hypothetical time domain vector, length(y)=N
@@ -36,14 +40,25 @@ PulsePressureFFT_real = PulsePressureFFT(1:floor(end*0.9));
 % Find the center frequency according to FDA 510(k) guidelines (see 510(k) definitions of bandwidth, center frequency)
 % The last 10% frequency bin is not included in f2 case the signal is over sampled.
 f1i = find(abs(PulsePressureFFT_real)>max(abs(PulsePressureFFT_real))*0.71, 1,'first'); % 0.71 is -3 dB
-f2i = find(abs(PulsePressureFFT_real(1:end-1))>max(abs(PulsePressureFFT_real))*0.71, 1,'last');
+f2i = find(abs(PulsePressureFFT_real(1:end-1))>(max(abs(PulsePressureFFT_real))*0.71), 1,'last');
 
 f1 = FrequencySpectrum(f1i);
 f2 = FrequencySpectrum(f2i);
-
-PulseBandwidth = f2-f1;
 fc = (f2+f1)/2;
+PulseBandwidth = f2-f1;
+
+% Fix for really narrowband signals
+if f1==f2 && PulseBandwidth == 0
+    f1b = interp1(abs(PulsePressureFFT_real((f1i-1):f1i)), FrequencySpectrum((f1i-1):f1i), max(abs(PulsePressureFFT_real))*0.71);
+    f2b = interp1(abs(PulsePressureFFT_real(f2i:(f2i+1))), FrequencySpectrum(f2i:(f2i+1)), max(abs(PulsePressureFFT_real))*0.71);
+    PulseBandwidth=f2b-f1b;
+    fc = (f2b+f1b)/2;
+end
+
+
 varargout{3} = fc;
+
+varargout{4} = PulseBandwidth;
 
 
 
